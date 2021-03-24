c0e3565e5a9d51523c41fccd79b03d21acdfa4f8
dhviet12 <73893345+dhviet12@users.noreply.github.com>
2021-03-24 16:05:18 +0700

    add create for account

--- src/main/java/com/example/demo/controller/AccountController.java
+++ src/main/java/com/example/demo/controller/AccountController.java
@@ -1,11 +1,13 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.login.AppUser;
 import com.example.demo.service.login.IAppUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -18,6 +20,11 @@ public class AccountController {
     @GetMapping()
     public ModelAndView showAll() {
         return new ModelAndView("account/list", "list", appUserService.findAll());
+    }
+
+    @GetMapping("/create")
+    public ModelAndView showFormCreate(){
+        return new ModelAndView("account/create","account",new AppUser());
     }
 
 
