84eec768c3b2d1d26dab772aae9bbcf90a3e38ed
dhviet12 <73893345+dhviet12@users.noreply.github.com>
2021-03-24 15:46:23 +0700

    add show List Account

--- src/main/java/com/example/demo/controller/AccountController.java
+++ src/main/java/com/example/demo/controller/AccountController.java
@@ -1,7 +1,24 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.login.IAppUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/account")
 public class AccountController {
+    @Autowired
+    IAppUserService appUserService;
+
+    @GetMapping()
+    public ModelAndView showAll() {
+        return new ModelAndView("account/list", "list", appUserService.findAll());
+    }
+
+
 }
