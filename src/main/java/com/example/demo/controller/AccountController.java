5705993378359341b407ec2da3b0e1bf64395ac3
dhviet12 <73893345+dhviet12@users.noreply.github.com>
2021-03-24 16:08:59 +0700

    done post mappingfor create

--- src/main/java/com/example/demo/controller/AccountController.java
+++ src/main/java/com/example/demo/controller/AccountController.java
@@ -8,6 +8,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -25,6 +26,11 @@ public class AccountController {
     @GetMapping("/create")
     public ModelAndView showFormCreate(){
         return new ModelAndView("account/create","account",new AppUser());
+    }
+    @PostMapping("create")
+    public ModelAndView createAccount(@ModelAttribute AppUser appUser){
+        appUserService.save(appUser);
+        return new ModelAndView("redirect:/account");
     }
 
 
