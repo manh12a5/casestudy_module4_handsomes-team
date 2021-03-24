93c1d4ce3336b54d5cd01905688453acfa90f770
dhviet12 <73893345+dhviet12@users.noreply.github.com>
2021-03-24 13:03:48 +0700

    create AppSecurityConfig and AppSecurityInit with no code

---
+++ src/main/java/com/example/demo/config/AppSecurityInit.java
@@ -0,0 +1,6 @@
+package com.example.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class AppSecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
