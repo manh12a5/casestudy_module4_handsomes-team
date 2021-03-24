557c0207c265fe06c93df132dd9448319b2319b1
dhviet12 <73893345+dhviet12@users.noreply.github.com>
2021-03-24 14:58:23 +0700

    test

--- src/main/java/com/example/demo/repository/AppUserRepository.java
+++ src/main/java/com/example/demo/repository/AppUserRepository.java
@@ -2,6 +2,10 @@ package com.example.demo.repository;
 
 import com.example.demo.model.login.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+    AppUser getAppUserByUsername(String username);
+
 }
--- src/main/java/com/example/demo/service/login/IAppUserService.java
+++ src/main/java/com/example/demo/service/login/IAppUserService.java
@@ -1,2 +1,9 @@
-package com.example.demo.service.login;public interface IAppUserService {
+package com.example.demo.service.login;
+
+import com.example.demo.model.login.AppUser;
+import com.example.demo.service.IService;
+
+public interface IAppUserService extends IService<AppUser> {
+    AppUser getAccountByUserName(String username);
+    AppUser getCurrentAccount();
 }
