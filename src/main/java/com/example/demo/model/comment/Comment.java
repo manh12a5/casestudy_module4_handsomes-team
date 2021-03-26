5e27f8f3b2e528d02136b5ecc464d16846e70c8b
doan tung <doantung2401@gmail.com>
2021-03-26 09:44:45 +0700

    create comment

--- src/main/java/com/example/demo/model/comment/Comment.java
+++ src/main/java/com/example/demo/model/comment/Comment.java
@@ -21,4 +21,55 @@ public class Comment {
     private Product product;
 
     private Timestamp date;
+
+    public Comment() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public Comment(Long id, String comment, AppUser user, Product product, Timestamp date) {
+        this.id = id;
+        this.comment = comment;
+        this.user = user;
+        this.product = product;
+        this.date = date;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public AppUser getUser() {
+        return user;
+    }
+
+    public void setUser(AppUser user) {
+        this.user = user;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Timestamp getDate() {
+        return date;
+    }
+
+    public void setDate(Timestamp date) {
+        this.date = date;
+    }
 }
